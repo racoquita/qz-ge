a8601bc9ee97843541d1e2d5a960402c1b39f93a
Richard Stovall <rstovall@me.com>
2014-06-06 20:47:08 -0400

    oh lord

--- innovation/2014-06-02/desktop/js/app.js
+++ innovation/2014-06-02/desktop/js/app.js
@@ -1,204 +1,171 @@
-// var App = function() {
-// 	var that = this;
-
-// 	this.on = function() {
-
-// 	}
-// 	this.off = function() {
-
-// 	}
-// };
-
-// Global variables
-var items = [];
-var startItem = 1;
-var position = 0;
-var itemCount = $('.carousel li.items').length;
-var leftpos = itemCount;
-var resetCount = itemCount;
-var autoAdvance;
-
 function layout() {
-	var wrapperWidth = $('.wrapper').width();
+	var wrapperWidth = $('#qzad').width();
 	var carouselOffset = Math.round((wrapperWidth/2) - 360);
 	$('.carousel').css('padding-left', carouselOffset);
 }
 
-$(window).on('resize', function(){
+var App = function() {
+	var that = this;
+	var items = [];
+	var startItem = 1;
+	var position = 0;
+	var itemCount = $('.carousel li.items').length;
+	var leftpos = itemCount;
+	var resetCount = itemCount;
+	var autoAdvance;
+
+	(function($) {
+		$.fn.swipeEvents = function() {
+			return this.each(function() {
+
+				var startX, startY, $this = $(this);
+
+				$this.bind('touchstart', touchstart);
+
+				function touchstart(event) {
+					var touches = event.originalEvent.touches;
+					if (touches && touches.length) {
+						startX = touches[0].pageX;
+						startY = touches[0].pageY;
+						$this.bind('touchmove', touchmove);
+						$this.bind('touchend', touchend);
+					}
+					event.preventDefault();
+				}
+
+				function touchmove(event) {
+					var touches = event.originalEvent.touches;
+					if (touches && touches.length) {
+						var deltaX = startX - touches[0].pageX;
+						var deltaY = startY - touches[0].pageY;
+
+						if (deltaX >= 50) {
+							$this.trigger("swipeLeft");
+						}
+						if (deltaX <= -50) {
+							$this.trigger("swipeRight");
+						}
+						if (deltaY >= 50) {
+							$this.trigger("swipeUp");
+						}
+						if (deltaY <= -50) {
+							$this.trigger("swipeDown");
+						}
+						if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+							$this.unbind('touchmove', touchmove);
+							$this.unbind('touchend', touchend);
+						}
+					}
+				}
+
+				function touchend(event) {
+					$this.unbind('touchmove', touchmove);
+					event.preventDefault();
+				}
+			});
+		};
+	})(jQuery);
+
+	this.on = function() {
+		$(window).on('resize', function(){
+			layout();
+		});
+
+		autoAdvance = setTimeout(that.swap, 2500);
+
+		$('#next').click(function() {
+			that.swap('clockwise');
+		});
+
+		$('#prev').click(function() {
+			that.swap('counter-clockwise');
+		});
+
+		$('li').click(function() {
+			if($(this).attr('class') == 'items left-pos') {
+				that.swap('counter-clockwise');
+			} else {
+				that.swap('clockwise');
+			}
+
+			QZIX.manualTrigger('internal', 'click', 'panel clicked', false);
+		});
+	}
+	this.off = function() {
+		$('#next').off();
+		$('#prev').off();
+		$('li').off();
+
+		$('#1').removeClass().addClass('items main-pos');
+		$('#2').removeClass().addClass('items right-pos');
+		$('#3, #4, #5, #6, #7, #8').removeClass().addClass('items back-pos');
+		$('#prev').addClass('hide');
+
+		startItem = 1;
+		position = 0;
+	}
+	this.swap = function(action) {
+		var direction = action;
+
+		if(direction == 'counter-clockwise') {
+			var leftitem = $('.left-pos').attr('id') - 1;
+
+			if(leftitem >= 0) {
+				$('.right-pos').removeClass('right-pos').addClass('back-pos');
+				$('.main-pos').removeClass('main-pos').addClass('right-pos');
+				$('.left-pos').removeClass('left-pos').addClass('main-pos');
+				$('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
+
+				startItem--;
+				if(startItem < 1) {
+					startItem = itemCount;
+				}
+			}
+			if(startItem == 1){
+				$('#prev').addClass('hide');
+			}
+			if(startItem < itemCount) {
+				$('#next').removeClass('hide');
+			}
+		}
+
+		if(direction == 'clockwise' || direction == '' || direction == null ) {
+			clearTimeout(autoAdvance);
+			function pos(positionvalue) {
+				if(positionvalue != 'leftposition') {
+					position++;
+				}
+
+				if(positionvalue == 'leftposition') {
+					position = startItem - 1;
+
+					if(position < 1) {
+						position = itemCount;
+					}
+				}
+				return position;
+			}
+
+			if (startItem < itemCount){
+				$('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
+				$('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
+				$('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
+				$('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
+
+				startItem++;
+				position = 0;
+
+			}
+			if(startItem > 1){
+				$('#prev').removeClass('hide');
+			}
+			if(startItem >= itemCount) {
+				$('#next').addClass('hide');
+			}
+		}
+	}
+};
+
+$(document).ready(function(){
 	layout();
 });
-
-layout();
-autoAdvance = setTimeout(swap, 2500);
-
-// Swap images function
-function swap(action) {
-	//$('.main-pos').unbind('swipeLeft').unbind('swipeRight');
-	var direction = action;
-
-	// Moving carousel backwards
-	if(direction == 'counter-clockwise') {
-		var leftitem = $('.left-pos').attr('id') - 1;
-		// Allows slides to wrap around
-		// if(leftitem == 0) {
-		//    leftitem = itemCount;
-		// }
-
-		if(leftitem >= 0) {
-			$('.right-pos').removeClass('right-pos').addClass('back-pos');
-			$('.main-pos').removeClass('main-pos').addClass('right-pos');
-			$('.left-pos').removeClass('left-pos').addClass('main-pos');
-			$('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
-
-			startItem--;
-			if(startItem < 1) {
-				startItem = itemCount;
-			}
-		}
-		if(startItem == 1){
-			$('#prev').addClass('hide');
-		}
-		if(startItem < itemCount) {
-			$('#next').removeClass('hide');
-		}
-	}
-
-	// Moving carousel forward
-	if(direction == 'clockwise' || direction == '' || direction == null ) {
-		clearTimeout(autoAdvance);
-		function pos(positionvalue) {
-			if(positionvalue != 'leftposition') {
-				// Increment image list id
-				position++;
-
-				// If final result is greater than image count, reset position.
-				// if((startItem+position) > resetCount) {
-				// 	position = 1-startItem;
-				// }
-			}
-
-			// Setting the left positioned item
-			if(positionvalue == 'leftposition') {
-				// Left positioned image should always be one left than main positioned image.
-				position = startItem - 1;
-
-				// Reset last image in list to left position if first image is in main position
-				if(position < 1) {
-					position = itemCount;
-				}
-			}
-
-			return position;
-		}
-
-		if (startItem < itemCount){
-			$('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
-			$('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
-			$('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
-			$('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
-
-			startItem++;
-			position=0;
-
-			// Wraps slides back to start
-			// if(startItem > itemCount) {
-			// 	startItem = 1;
-			// }
-		}
-		if(startItem > 1){
-			$('#prev').removeClass('hide');
-		}
-		if(startItem >= itemCount) {
-			$('#next').addClass('hide');
-		}
-
-	}
-	// bindSwipes();
-}
-
-// Next button click function
-$('#next').click(function() {
-	swap('clockwise');
-});
-
-// Prev button click function
-$('#prev').click(function() {
-	swap('counter-clockwise');
-});
-
-// If any visible items are clicked
-$('li').click(function() {
-	if($(this).attr('class') == 'items left-pos') {
-		swap('counter-clockwise');
-	} else {
-		swap('clockwise');
-	}
-});
-
-// Swipe events
-// $(function(){
-// 	bindSwipes();
-// });
-
-// function bindSwipes() {
-// 	$('.main-pos')
-// 		.swipeEvents()
-// 		.bind("swipeLeft",  function(){ swap('clockwise'); })
-// 		.bind("swipeRight", function(){ swap('counter-clockwise'); });
-// }
-
-// Capture swipe events
-(function($) {
-	$.fn.swipeEvents = function() {
-		return this.each(function() {
-
-			var startX,
-				startY,
-				$this = $(this);
-
-			$this.bind('touchstart', touchstart);
-
-			function touchstart(event) {
-				var touches = event.originalEvent.touches;
-				if (touches && touches.length) {
-					startX = touches[0].pageX;
-					startY = touches[0].pageY;
-					$this.bind('touchmove', touchmove);
-					$this.bind('touchend', touchend);
-				}
-				event.preventDefault();
-			}
-
-			function touchmove(event) {
-				var touches = event.originalEvent.touches;
-				if (touches && touches.length) {
-					var deltaX = startX - touches[0].pageX;
-					var deltaY = startY - touches[0].pageY;
-
-					if (deltaX >= 50) {
-						$this.trigger("swipeLeft");
-					}
-					if (deltaX <= -50) {
-						$this.trigger("swipeRight");
-					}
-					if (deltaY >= 50) {
-						$this.trigger("swipeUp");
-					}
-					if (deltaY <= -50) {
-						$this.trigger("swipeDown");
-					}
-					if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
-						$this.unbind('touchmove', touchmove);
-						$this.unbind('touchend', touchend);
-					}
-				}
-				//event.preventDefault();
-			}
-
-			function touchend(event) {
-				$this.unbind('touchmove', touchmove);
-				event.preventDefault();
-			}
-		});
-	};
-})(jQuery);
--- innovation/2014-06-02/mobile/js/app.js
+++ innovation/2014-06-02/mobile/js/app.js
@@ -1,111 +1,96 @@
 var App = function() {
-	var that = this; // a little of this; a little of that.
+	var that = this;
+	var cp = 0;
+	var items = $('.items');
+	var introTimer;
 
-  // protected variables
-  var cp = 0;
-  var items = $('.items');
-  var introTimer;
+	var move = function ( direction ) {
 
-  // the move function does nearly all of the required work.
-  var move = function ( direction ) {
+		clearTimeout(introTimer);
 
-    clearTimeout(introTimer);
+		if (direction === 'left') {
+			cp++;
+			if (cp > items.length - 1) {
+				cp = items.length - 1;
+			}
+		}
+		else if (direction === 'right') {
+			cp--;
+			if (cp < 0) {
+				cp = 0;
+			}
+		}
 
-    // check direction and incriment the cp variable accordingly.
-    if (direction === 'left') {
-      cp++;
-      if (cp > items.length - 1) {
-        cp = items.length - 1;
-      }
-    }
-    else if (direction === 'right') {
-      cp--;
-      if (cp < 0) {
-        cp = 0;
-      }
-    }
+		var ondeck = items.eq(cp+2);
+		
+		if (ondeck.length > 0 && !ondeck.data('loaded')) {
+			ondeck.data('loaded', true);
+			ondeck.find('img').attr( 'src', ondeck.data('lazy-src'));
+		}
 
-    var ondeck = items.eq(cp+2);
-    if (ondeck.length > 0 && !ondeck.data('loaded')) {
-      ondeck.data('loaded', true);
-      ondeck.find('img').attr( 'src', ondeck.data('lazy-src'));
-    }
+		items.removeClass('back');
+		items.removeClass('left');
+		items.removeClass('right');
+		items.removeClass('active');
+		items.eq(cp).addClass('active');
 
-    // prep
-    items.removeClass('back');
-    items.removeClass('left');
-    items.removeClass('right');
-    items.removeClass('active');
+		if (cp === 0 ) {
+			lp = false;
+			$('.left-arrow').addClass('hide');
+		}
+		else {
+			items.eq(cp-1).addClass('left');
+			$('.left-arrow').removeClass('hide');
+		}
 
+		if (cp === items.length - 1) {
+			$('.right-arrow').addClass('hide');
+		}
+		else {
+			items.eq(cp+1).addClass('right');
+			$('.right-arrow').removeClass('hide'); 
+		}
 
-    // deal with the active slide.
-    items.eq(cp).addClass('active');
+		items.each(function () {
+			if (!$(this).hasClass('active') && !$(this).hasClass('left') && !$(this).hasClass('right')) {
+				$(this).addClass('back');
+			}
+		});
+	};
 
-    // check if user is at left bound.
-    if (cp === 0 ) {
-      lp = false;
-      $('.left-arrow').addClass('hide');
-    }
-    // if they're not, then business as usual.
-    else {
-      items.eq(cp-1).addClass('left');
-      $('.left-arrow').removeClass('hide');
-    }
-
-    // check if user is at right bound.
-    // if they are, hide the right arrow.
-    if (cp === items.length - 1) {
-      $('.right-arrow').addClass('hide');
-    }
-    // if they're not, then business as usual.
-    else {
-      items.eq(cp+1).addClass('right');
-      $('.right-arrow').removeClass('hide'); 
-    }
-
-    // deal with the back items
-    items.each(function () {
-      if (!$(this).hasClass('active') && !$(this).hasClass('left') && !$(this).hasClass('right')) {
-        $(this).addClass('back');
-      }
-    })
-  };
-
-
-  var bindToSwipeEvents = function () {
-
-    var startX
-    ,   ul = $('ul.carousel');
-    var down = function (e) {
-      startX = e.clientX;
-      ul.on('vmouseup', up);
-    };
-
-    var up = function (e) {
-      if (startX - e.clientX > 50) {
-        move('left');
-      } else if (startX - e.clientX < -50) {
-        move('right');
-      }
-      ul.off('vmouseup', up);
-    }
-    ul.on('vmousedown', down );
-  };
-
+	this.bindSwipe = function(){
+		$('li').swipe({
+			swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+				if(direction == 'left') move('left');
+				if(direction == 'right') move('right');
+			}
+		});
+	}
 
 	this.on = function() {
-    bindToSwipeEvents();
-    $('.left-arrow').on('click', function (e) {
-      move('right');
-    });
-    $('.right-arrow').on('click', function (e) {
-      move('left');
-    });
-    introTimer = setTimeout(function () {
-      move('left')
-    }, 2000);
+		that.bindSwipe();
+
+		$('.left-arrow').on('click', function (e) {
+			move('right');
+		});
+		$('.right-arrow').on('click', function (e) {
+			move('left');
+		});
+		introTimer = setTimeout(function () {
+			move('left')
+		}, 2000);
 	}
 	this.off = function() {
+		$('#next').off();
+		$('#prev').off();
+		 clearTimeout(introTimer);
 
+		$('#1').removeClass().addClass('items active');
+		$('#2').removeClass().addClass('items right');
+		$('#3, #4, #5, #6, #7, #8').removeClass().addClass('items back');
+		$('.left-arrow').off().addClass('hide');
+		$('.right-arrow').off().removeClass('hide');
+
+		cp = 0;
 	}
 };
