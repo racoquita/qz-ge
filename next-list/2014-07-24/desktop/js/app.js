4606500d41f6e3110f88baa2307c744ad036f599
Richard Stovall <rstovall@me.com>
2014-07-25 11:26:51 -0400

    adding the subtle animations - fml

--- next-list/2014-07-24/desktop/js/app.js
+++ next-list/2014-07-24/desktop/js/app.js
@@ -2,7 +2,11 @@ var App = function() {
 	var that = this;
 
 	this.on = function() {
-
+		$('li.item').on('mouseenter', function(e){
+			$(e.currentTarget).addClass('active');
+		}).on('mouseleave', function(e){
+			$(e.currentTarget).removeClass('active');
+		});
 	}
 	this.off = function() {
 
