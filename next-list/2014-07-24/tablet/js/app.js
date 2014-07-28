555776d76524772fd76bf8660b6c56a173f048e7
Richard Stovall <rstovall@me.com>
2014-07-28 15:37:05 -0400

    a few modifications to the tablet unit

--- next-list/2014-07-24/tablet/js/app.js
+++ next-list/2014-07-24/tablet/js/app.js
@@ -4,14 +4,24 @@ var App = function() {
 	this.on = function() {
 		$('li.item').on('click', function(e){
 			$('.showcase[data-id="'+ $(e.currentTarget).data('id') +'"]').show();
+			$('.bg').css({
+				'background': 'url(../images/bg-blur.png) no-repeat center bottom',
+				'background-size': '100% 100%'
+			});
+			$('.wrapper').hide();
 		});
 
 		$('.x').on('click', function(){
 			$('.showcase').hide();
+			$('.bg').css({
+				'background': 'url(../images/bg.png) no-repeat center bottom',
+				'background-size': '100% 100%'
+			});
 		});
 	}
 	this.off = function() {
 		$('li.item').off();
+		$('.wrapper').show();
 		$('.showcase').hide();
 	}
 };
