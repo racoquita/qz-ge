9c551604accf07dcbf91856e48a93dffcb136491
Carolina Yibirin-Stovall <racoquita@codey.local>
2014-08-27 18:19:47 -0400

    Almost done Desktop_tablet GE Oil and Gas

--- oil-and-gas/desktop_tablet/js/app.js
+++ oil-and-gas/desktop_tablet/js/app.js
@@ -2,7 +2,17 @@ var App = function() {
 	var that = this;
 
 	this.on = function() {
+		$('.tooltip .discoverCta').on('click', function (e) {
+			var target = $(this).attr('href');
 
+			$('.tier').removeClass('show').addClass('hide');
+			$(target).addClass('show');
+			e.preventDefault();
+		});
+		$('.close').on('click', function(e){
+			$(this).parent().removeClass('show').addClass('hide');
+			e.preventDefault();
+		})
 	}
 	this.off = function() {
 
